00daaf70d7884b22bba129db4083dcf3c98b0f6c
alexander-laputko <alexander.laputko@ya.ru>
2022-07-29 15:17:48 +0300

    Two new string resources

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -55,8 +58,20 @@ fun BirthdayGreetingWithImage(message: String, from: String) {
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
     Column {
-        Text(text = message, fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from, fontSize = 24.sp, modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+        )
     }
 }
 
@@ -64,6 +79,9 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 @Composable
 fun BirthdayGreetingWithImagePreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithImage(message = "Happy Birthday, Alexander!", from = "-from Grigorii")
+        BirthdayGreetingWithImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.from_greeting_text)
+        )
     }
 }
